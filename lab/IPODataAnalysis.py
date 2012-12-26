93591b1b260cedcb9a75ad1f16ee94701b473c11
Ash <as17237@gmail.com>
2012-12-25 20:25:37 -0500

    formatting changes and package changes

--- lab/IPODataAnalysis.py
+++ lab/IPODataAnalysis.py
@@ -9,28 +9,19 @@ months = ["2010-01", "2010-02", "2010-03", "2010-04", "2010-05", "2010-06", "201
           "2011-09", "2011-10", "2011-01", "2011-12", "2012-01", "2012-02", "2012-03", "2012-04", "2012-05", "2012-06",
           "2012-07", "2012-08", "2012-09", "2012-10", "2012-01", "2012-12"]
 
-urlList = [baseURL+month for month in months]
+urlList = [baseURL + month for month in months]
 pageList = [urllib2.urlopen(x).read() for x in urlList]
 soupList = [BeautifulSoup(page) for page in pageList]
 for soup in soupList:
     count = 0
     prntCnt = 0
     for link in soup.find_all('td'):
-        if(count<4):
-            print('')
+        if (count < 4):
+            print(''),
         else:
-            print(link.get_text()+'|'),
+            print(link.get_text() + '|'),
             prntCnt += 1
-            if(prntCnt%7 == 0):
+            if (prntCnt % 7 == 0):
                 print('')
         count += 1
 
-
-
-
-
-#p = html5lib.HTMLParser()
-
-str = urllib2.urlopen("http://www.nasdaq.com/markets/ipos/activity.aspx?tab=pricings&month=2010-01").read()
-
-soup = BeautifulSoup(str)
