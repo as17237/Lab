e7f5a316fbe2fe510ea6691d72805ae2650e1111
as17237 <as17237@gmail.com>
2013-02-24 18:34:01 -0500

    Removed the hardcoded alphabet

--- lab/BabyNames.py
+++ lab/BabyNames.py
@@ -3,7 +3,7 @@ __author__ = 'as17237'
 import urllib2
 from bs4 import BeautifulSoup
 import pprint
-
+import string
 
 baseBoyURL = "http://www.indianhindunames.com/indian-hindu-boy-name-"
 baseGirlURL = "http://www.indianhindunames.com/indian-hindu-girl-name-"
@@ -13,9 +13,7 @@ def extractNames(url):
     nameDict = {}
     pp = pprint.PrettyPrinter(indent=4)
     global alphabet, letter, urlList, x, pageList, page, soupList, sectionList, section, fragmentList, fragment, item
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z"]
-    urlList = [url + letter + ".htm" for letter in alphabet]
+    urlList = [url + letter + ".htm" for letter in string.lowercase]
     pageList = [urllib2.urlopen(x).read() for x in urlList]
     soupList = [BeautifulSoup(page) for page in pageList]
     sectionList = [x.find("div", {"id": "content_child"}) for x in soupList]
